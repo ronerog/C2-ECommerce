5ec30cf30b6dffd715b4b5096d7f937bfe9bb4cf
Rônero <52432170+ronerog@users.noreply.github.com>
2024-02-23 10:28:20 -0300

    style: css login

--- c2-ecommerce/src/screens/Login/Login.js
+++ c2-ecommerce/src/screens/Login/Login.js
@@ -17,10 +17,9 @@ export const Login = () => {
                 alt="Icone"
                  />
                 </div> */}
-                <div className="centerdiv">
                 <div className="text-wrapper">Seja bem-vindo</div>
                 <div className="text-wrapper-2">Faça seu Login</div>
-                <input className="overlap" name="email" type="email" placeholder="Email" />
+                <input className="overlap-group" name="email" type="email" placeholder="Email" />
                 <input className="overlap-group" name="senha" type="password" placeholder="Senha" />
                 <div><button type="button" onClick={HandleClick} className="div-wrapper" >Entrar</button></div>
                 <div>
@@ -31,8 +30,5 @@ export const Login = () => {
                 </div>
                 </div>
             </div>
-            
-            
-        </div>
     );
 }
