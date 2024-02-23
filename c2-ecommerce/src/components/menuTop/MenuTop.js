6c09ad642b141b07051bf69f18fb85abab22e14b
Rônero <52432170+ronerog@users.noreply.github.com>
2024-02-23 01:52:39 -0300

    add logic to login>logout

--- c2-ecommerce/src/components/menuTop/MenuTop.js
+++ c2-ecommerce/src/components/menuTop/MenuTop.js
@@ -1,14 +1,24 @@
 'use client';
 import styles from "./menuTop.css";
-import React from "react";
+import React, { useState } from "react";
 
 export const MenuTop = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLogin = () => {
+        setIsLoggedIn(!isLoggedIn);
+    };
+
     return (
         <div className="topnav">
-        <a className="active" href="#home">Entrar</a>
-        <a href="#news">Planos e Precos</a>
-        <a href="#contact">Contato</a>
-        <a href="#about">About</a>
-      </div>
+            {isLoggedIn ? (
+                <a href="#logout" onClick={handleLogin}>Sair</a>
+            ) : (
+                <a className="active" href="#login" onClick={handleLogin}>Entrar</a>
+            )}
+            <a href="#news">Planos e Preços</a>
+            <a href="#contact">Contato</a>
+            <a href="#about">Sobre</a>
+        </div>
     );
-  }
+}
